229d1398a6c5abf3b46f74f88e75f1c96a77f4da
inkaaulia0524 <inkaaulia2540@gmail.com>
2025-09-29 13:52:33 +0700

    uts:jawaban5

---
+++ sistem-toko/resources/views/uts/index.blade.php
@@ -0,0 +1,6 @@
+@extends('uts.layout')
+
+@section('content')
+    <h2>Selamat datang di halaman UTS</h2>
+    <p>Pilih menu di atas untuk melihat konten.</p>
+@endsection
---
+++ sistem-toko/resources/views/uts/pemrograman.blade.php
@@ -0,0 +1,6 @@
+@extends('uts.layout')
+
+@section('content')
+    <h2>Menu UTS Pemrograman Web</h2>
+    <p>Ini adalah halaman khusus untuk pemrograman web.</p>
+@endsection
